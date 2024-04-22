aa9a248a65f2ba070ac66c0a0b2c66d3a4aa35d5
boulhouech <slim.boulahouech@gmail.com>
2024-04-22 18:01:40 +0100

    Update the best days to buy and sell if the profit is greater

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,6 +2,7 @@ def stock_picker(prices)
   min_price = prices[0]
   min_price_index = 0
   max_price = 0
+  max_profit
   best_days = [nil, nil]
 
   prices.each_with_index do |price, index|
@@ -10,4 +11,14 @@ def stock_picker(prices)
       min_price = price
       min_price_index = index
   end
+
+  profit = price - min_price
+
+  if profit > max_profit
+    max_profit = profit
+    best_days[min_price_index, index]
+
+
+
+
 end
