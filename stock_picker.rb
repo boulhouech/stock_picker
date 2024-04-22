8ee2edc80eaf3acf6182d6da34ebf16d888820ef
boulhouech <slim.boulahouech@gmail.com>
2024-04-22 17:39:47 +0100

    Initialize variables to keep track of the best days to buy and sell

---
+++ stock_picker.rb
@@ -0,0 +1,11 @@
+def stock_picker(prices)
+  min_price = prices[0]
+  min_price_index = 0
+  max_price = 0
+  best_days = [nil, nil]
+
+  prices.each_with_index do |price, index|
+    if
+
+  end
+end
